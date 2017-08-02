170f3c0ee59fadf1294e986770c65bce79573ee0
Jiri Dohnalek <thinkabout3e@googlemail.com>
2017-08-02 17:11:17 +0100

    Update hx711.py

--- hx711.py
+++ hx711.py
@@ -1,3 +1,24 @@
+"""
+HX711 Load cell amplifier Python Library
+Original: https://gist.github.com/underdoeg/98a38b54f889fce2b237
+Copyright (C) 2017 Jiri Dohnalek
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+
 import RPi.GPIO as GPIO
 import time
 import sys
@@ -5,49 +26,74 @@ import sys
 
 class HX711:
 
-    
     def __init__(self, dout, pd_sck, gain=128):
-        
-        # Set GPIO Numbering mode
-        GPIO.setmode(GPIO.BCM)
-
-        self.PD_SCK = pd_sck
-        self.DOUT = dout
-
-        GPIO.setup(self.PD_SCK, GPIO.OUT)
-        GPIO.setup(self.DOUT, GPIO.IN)
-
+        """
+        Set GPIO Mode, and pin for communication with HX711
+        :param dout: Serial Data Output pin
+        :param pd_sck: Power Down and Serial Clock Input pin
+        :param gain: set gain 128, 64, 32
+        """
         self.GAIN = 0
         self.OFFSET = 0
         self.SCALE = 1
-        self.lastVal = 0
 
-        GPIO.output(self.PD_SCK, True)
-        GPIO.output(self.PD_SCK, False)
-
-        self.set_gain(gain);
-
-    
-    def is_ready(self):
-        return GPIO.input(self.DOUT) == 0
-
-
-    def set_gain(self, gain):
-        if gain is 128:
+        try:
+            if gain is 128:
+                self.GAIN = 1
+            elif gain is 64:
+                self.GAIN = 3
+            elif gain is 32:
+                self.GAIN = 2
+        except:
             self.GAIN = 1
-        elif gain is 64:
-            self.GAIN = 3
-        elif gain is 32:
-            self.GAIN = 2
 
-        GPIO.output(self.PD_SCK, False)
-        
+        # Setup the gpio pin numbering system
+        GPIO.setmode(GPIO.BCM)
+
+        # Set the pin numbers
+        self.PD_SCK = pd_sck
+        self.DOUT = dout
+
+        # Setup the GPIO Pin as output
+        GPIO.setup(self.PD_SCK, GPIO.OUT)
+
+        # Setup the GPIO Pin as input
+        GPIO.setup(self.DOUT, GPIO.IN)
+
+        # Power up the chip
+        self.power_up()
+
+
+    def set_scale(self, scale):
+        """
+        Set scale
+        :param scale, scale
+        """
+        self.SCALE = scale
+
+
+    def set_offset(self, offset):
+        """
+        Set the offset
+        :param offset: offset
+        """
+        self.OFFSET = offset
+
 
     def read(self):
+        """
+        Read data from the HX711 chip
+        :param void
+        """
 
-        while not self.is_ready():
+        # Control if the output is ready
+        while not (GPIO.input(self.DOUT) == 0):
             pass
 
+        # Original C source code ported to python as described in datasheet
+        # https://cdn.sparkfun.com/datasheets/Sensors/ForceFlex/hx711_english.pdf
+        # Output matched to output on while running Arduino HX711 library example
+        # Also, behaviour matches while applying pressure
         count = 0
         for i in range(24):
 
@@ -59,61 +105,45 @@ class HX711:
 
         return count ^ 0x800000
 
-    
-    def read_average(self, times=3):
+
+    def read_average(self, times=16):
+        """
+        Calculate average value from
+        :param times: measure x amount of time to get average
+        """
         sum = 0
         for i in range(times):
             sum += self.read()
         return sum / times
-        
 
-    def get_value(self, times=3):
+
+    def get_grams(times=16):
+        """
+        :param times: set value to calculate average
+        :return float weight in grams
+        """
         return self.read_average(times) - self.OFFSET
 
 
-    def get_units(self, times=3):
-        return self.get_value(times) / self.SCALE
-
-
-    def tare(self, times=15):
+    def tare(self, times=16):
+        """
+        Tare functionality
+        :param times: set value to calculate average
+        """
         sum = self.read_average(times)
         self.set_offset(sum)
 
 
-    def set_scale(self, scale):
-        self.SCALE = scale
-
-
-    def set_offset(self, offset):
-        self.OFFSET = offset
-
-
     def power_down(self):
+        """
+        Power the chip down
+        """
         GPIO.output(self.PD_SCK, False)
         GPIO.output(self.PD_SCK, True)
 
 
     def power_up(self):
+        """
+        Power the chip up
+        """
         GPIO.output(self.PD_SCK, False)
-
-
-############# EXAMPLE
-
-hx = HX711(5, 6)
-hx.set_scale(24813)
-hx.tare()
-
-while True:
-    try:
-        val = hx.read_average()
-        print val
-
-        hx.power_down()
-        time.sleep(.001)
-        hx.power_up()
-
-        time.sleep(2)
-
-    except (KeyboardInterrupt, SystemExit):
-        GPIO.cleanup()
-        sys.exit()
