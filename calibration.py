3e1cadd15ae1ab1a431075253ba74ff6919ad31a
NeonSpork <leiknes@gmail.com>
2018-11-03 13:18:56 +0100

    added a comment in the header that calibrartion needs to run in python 3

--- calibration.py
+++ calibration.py
@@ -17,6 +17,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ATTENTION:
+This version runs in python 3.x (using python 2.7 will break it)
 """
 
 import RPi.GPIO as GPIO
